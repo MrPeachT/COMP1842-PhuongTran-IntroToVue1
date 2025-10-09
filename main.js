fcfa5fbe7075e326d808c9f31a75c51984a387c2
Tran Nguyen Phuong <138653567+MrPeachT@users.noreply.github.com>
2025-10-09 10:08:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            product_info: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf. Some types of shoes or boots are typically worn over socks. In ancient times, socks were made from leather or matted animal hair.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.com/search?q=socks&rlz=1C1GCEU_enGB832GB832&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjm5dOZ6v3-AhWQy4UKHfJdD0oQ_AUoAXoECAEQAw&biw=1536&bih=754&dpr=1.25',
+            inventory: 101,
+            onSale: true
+
+        }
+    }
+})
+
